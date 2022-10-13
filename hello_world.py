a6b915dffba8371b680484efefdd58c335026b14
nameunsol <106670648+nameunsol@users.noreply.github.com>
2022-10-13 16:01:03 +0900

    gamja2

--- hello_world.py
+++ hello_world.py
@@ -11,7 +11,7 @@ def print_hello():
     print("Hello World!!")
 
 
-print("남은솔 공주 웅냥냥")
+print("남은솔 공주")
 def print_KIT():
     print("kumoh")
     print("KIT")
@@ -19,5 +19,7 @@ if __name__ == '__main__':
     print_hello()
     print_KIT()
     print(print_hello(1,2))
+    a=0
+    print("a")
 
 
